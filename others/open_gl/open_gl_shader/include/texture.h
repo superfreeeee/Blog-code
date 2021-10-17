6b3d6d3d247229e714be0ec16f9f9cc270383704
superfree <superfreeeee@gmail.com>
2021-10-17 21:53:26 +0800

    add open_gl_shader

---
+++ others/open_gl/open_gl_shader/include/texture.h
@@ -0,0 +1,13 @@
+//
+// Created by 超悠閒 on 2021/10/17.
+//
+
+#ifndef OPEN_GL_TEMPLATE_TEXTURE_H
+#define OPEN_GL_TEMPLATE_TEXTURE_H
+
+#include <glad/glad.h>
+#include "shader.h"
+
+void loadTexture(const char *filename, GLuint *texture, int hasAlpha);
+
+#endif //OPEN_GL_TEMPLATE_TEXTURE_H
