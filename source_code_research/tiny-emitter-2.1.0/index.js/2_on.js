e6d2fdf65c823f4c29bbc285f2de705713382c17
superfree <superfreeeee@gmail.com>
2021-11-01 02:10:04 +0800

    add tiny-emitter-2.1.0

---
+++ source_code_research/tiny-emitter-2.1.0/index.js/2_on.js
@@ -0,0 +1,16 @@
+E.prototype = {
+  // ...
+
+  on: function (name, callback, ctx) {
+    var e = this.e || (this.e = {});
+
+    (e[name] || (e[name] = [])).push({
+      fn: callback,
+      ctx: ctx
+    });
+
+    return this;
+  },
+  
+  // ...
+};
---
+++ source_code_research/tiny-emitter-2.1.0/index.js/5_once.js
@@ -0,0 +1,16 @@
+E.prototype = {
+  // ...
+
+  once: function (name, callback, ctx) {
+    var self = this;
+    function listener () {
+      self.off(name, listener);
+      callback.apply(ctx, arguments);
+    };
+
+    listener._ = callback
+    return this.on(name, listener, ctx);
+  },
+  
+  // ...
+};
