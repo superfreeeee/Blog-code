8ddca759aad716a3795cc0848ae4b23885c911ed
superfree <superfreeeee@gmail.com>
2022-01-01 00:16:24 +0800

    add vue_vmodel_deep_dive

---
+++ front_end/vue/vue_vmodel_deep_dive/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <!-- <BasicVModel /> -->
+    <!-- <CustomComponent /> -->
+    <UnbindCustomComponent />
+  </div>
+</template>
+
+<script>
+import BasicVModel from './components/BasicVModel.vue';
+import CustomComponent from './components/CustomComponent.vue';
+import UnbindCustomComponent from './components/UnbindCustomComponent.vue';
+export default {
+  name: 'App',
+  components: { BasicVModel, CustomComponent, UnbindCustomComponent },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ front_end/vue/vue_vmodel_deep_dive/src/components/CustomComponentInput.vue
@@ -0,0 +1,12 @@
+<template>
+  <input :value="value" @input="$emit('input', $event.target.value)" />
+</template>
+
+<script>
+export default {
+  name: 'CustomComponentInput',
+  props: ['value'],
+};
+</script>
+
+<style></style>
